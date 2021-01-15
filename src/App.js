470415a9915d057cc6a5911801f03beee1377ba6
Thanawat <thanawat.t@dv.co.th>
2021-01-15 09:26:22 +0700

    OMG

--- src/App.js
+++ src/App.js
@@ -13,6 +13,40 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const { Engine, Render, World, Bodies, Mouse, MouseConstraint, Svg } = Matter;
+
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+function makePattern(pWidth) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width = canvas.height = pWidth || (10 + Math.random() * 20) >> 0;
+  ctx.fillStyle = randomColor();
+  if (Math.random() * 2 < 1) {
+    ctx.arc(
+      (canvas.width / 2) >> 0,
+      (canvas.width / 2) >> 0,
+      canvas.width * (Math.random() * 0.5),
+      0,
+      2 * Math.PI
+    );
+    ctx.fill();
+  } else {
+    var half = canvas.width / 2;
+    var lineHeight = (Math.random() * canvas.width) >> 0;
+    ctx.translate(half, half);
+    ctx.rotate((Math.random() * 90 * Math.PI) / 180);
+    ctx.fillRect(
+      -canvas.width,
+      (-lineHeight / 2) >> 0,
+      canvas.width * 2,
+      lineHeight
+    );
+  }
+  return ctx.createPattern(canvas, "repeat");
+}
+
 class Scene extends React.Component {
   constructor(props) {
     super(props);
@@ -20,18 +54,6 @@ class Scene extends React.Component {
   }
 
   componentDidMount() {
-    const {
-      Engine,
-      Render,
-      World,
-      Bodies,
-      Mouse,
-      MouseConstraint,
-      Svg,
-      Common,
-      Vertices,
-    } = Matter;
-
     const engine = Engine.create({
       // positionIterations: 20
     });
@@ -54,7 +76,9 @@ class Scene extends React.Component {
       restitution: 0.5,
     });
 
-    const ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
+    const ballB = Bodies.circle(110, 50, 30, {
+      restitution: 0.5,
+    });
     World.add(world, [
       // walls
 
@@ -70,31 +94,24 @@ class Scene extends React.Component {
     const data = ReactDOMServer.renderToStaticMarkup(A());
     console.log("data", data);
 
-    var vertexSets = [],
-      color = Common.choose([
-        "#f19648",
-        "#f5d259",
-        "#f55a3c",
-        "#063e7b",
-        "#ececd1",
-      ]);
+    const vertexSets = [];
 
     $(data)
       .find("path")
       .each(function (i, path) {
-        vertexSets.push(Svg.pathToVertices(path, 30));
+        vertexSets.push(Svg.pathToVertices(path, 100));
       });
 
     World.add(
       world,
       Bodies.fromVertices(
-        400,
-        80,
+        width / 2,
+        height / 2,
         vertexSets,
         {
           render: {
-            fillStyle: color,
-            strokeStyle: color,
+            fillStyle: makePattern(),
+            strokeStyle: randomColor(),
             lineWidth: 1,
           },
         },
